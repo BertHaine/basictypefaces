d9554315dcc8393c2e5b583e4fbba2b8217dd4d8
BertHaine <bertram.haine@gmail.com>
2017-07-13 14:41:04 -0700

    added favicon

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$('#fullpage').fullpage({
+  anchors: ['header', 'Garamond', 'Bodoni', 'Century Expanded', 'Futura', 'Times Roman', 'Helvetica'],
+  sectionsColor: ['#181818',],
+  csss3: true,
+  verticalCentered: true,
+  fadingEffect: true,
+  allowPageScroll: true,
+  paddingBottom: '0',
+  resize: true,
+  scrollOverflow: true,
+  easing: 'easeInOutCubic',
+  easingcss3: 'ease',
+  touchSensitivity: 3
+
+});
