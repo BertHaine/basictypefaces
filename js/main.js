2780596cb65550817d77c2188223e7e4a085756f
BertHaine <bertram.haine@gmail.com>
2017-07-13 18:25:38 -0700

    changed js colorbackground

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $('#fullpage').fullpage({
   anchors: ['header', 'Garamond', 'Bodoni', 'Century Expanded', 'Futura', 'Times Roman', 'Helvetica'],
-  sectionsColor: ['#181818', '#E9E5D4',],
+  sectionsColor: ['#181818', '#E9E5D4', '#E9E5D4', '#E9E5D4', '#E9E5D4', '#E9E5D4', '#E9E5D4' ],
   csss3: true,
   verticalCentered: true,
   fadingEffect: true,
